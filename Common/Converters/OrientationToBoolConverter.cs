3c767091fa2decc7eb72a3e6d1ec35666a8430a7
Peter Jones <944541960@qq.com>
2025-07-21 01:12:53 +0800

    修复滚动条样式

---
+++ Common/Converters/OrientationToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Orientation = System.Windows.Controls.Orientation;
+
+namespace DrugSearcher.Converters;
+
+/// <summary>
+/// Orientation为Vertical时返回true，Horizontal时返回false
+/// </summary>
+public class OrientationToBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Orientation orientation)
+        {
+            return orientation == Orientation.Vertical;
+        }
+        return false;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isVertical)
+        {
+            return isVertical ? Orientation.Vertical : Orientation.Horizontal;
+        }
+        return Orientation.Horizontal;
+    }
+}
